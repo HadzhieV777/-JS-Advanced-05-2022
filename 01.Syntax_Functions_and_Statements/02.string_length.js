f4ef13c5dd4c45df3dc5ef2e7a81428190d9c83f
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-05-18 10:55:45 +0300

    Added 02.string_length.js

---
+++ 01.Syntax_Functions_and_Statements/02.string_length.js
@@ -0,0 +1,12 @@
+ 
+function strlen(a, b, c) {
+  let total = a.length + b.length + c.length;
+  let avg = Math.floor(total / 3)
+
+  console.log(total)
+  console.log(avg)
+
+}
+
+strlen('chocolate', 'ice cream', 'cake')
+strlen('pasta', '5', '22.3')
