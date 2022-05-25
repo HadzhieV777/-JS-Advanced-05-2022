f20faa008b45e738fa94579c45c5e0e11f16b9d8
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-05-25 22:12:13 +0300

    Added 02.last_k_numbers.js

---
+++ 03.Arrays_and_Nested_Arrays/02.last_k_numbers_sequence.js
@@ -0,0 +1,20 @@
+function lastKNumbers(n, k) {
+  let result = [1];
+  for (let i = 1; i < n; i++) {
+    result[i] = sumLastK(result, k);
+  }
+
+  function sumLastK(array = result, k) {
+    k = array.length > k ? k : array.length;
+    let sum = 0;
+
+    for (let i = 1; i <= k; i++) {
+      sum += array[array.length - i];
+    }
+    return sum;
+  }
+  return result;
+}
+
+console.log(lastKNumbers(6, 3)); // [1, 1, 2, 4, 7, 13]
+console.log(lastKNumbers(8, 2)); // [1, 1, 2, 3, 5, 8, 13, 21]
