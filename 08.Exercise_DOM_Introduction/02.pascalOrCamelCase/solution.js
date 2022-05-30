89745b7cdf8fd43b1c898e63c342b7f4e53d300d
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-05-30 18:41:11 +0300

    Edited solution.js

--- 08.Exercise_DOM_Introduction/02.pascalOrCamelCase/solution.js
+++ 08.Exercise_DOM_Introduction/02.pascalOrCamelCase/solution.js
@@ -1,3 +1,38 @@
 function solve() {
-  //TODO...
+  let userInput = document.getElementById("text").value.split(" ");
+  let namingConvention = document.getElementById("naming-convention").value;
+  let result = "";
+
+  if (namingConvention === "Camel Case") {
+    result = camelCaseConversion(userInput);
+  } else if (namingConvention === "Pascal Case") {
+    result = pascalCaseConversion(userInput);
+  } else {
+    result = "Error!";
+  }
+
+  document.getElementById("result").textContent = result;
+
+  function camelCaseConversion(text) {
+    let result = text
+      .map((string) => string.toLowerCase())
+      .map((string, i) => {
+        if (i !== 0) {
+          return string[0].toUpperCase() + string.substring(1);
+        }
+        return string;
+      });
+
+    return result.join("");
+  }
+
+  function pascalCaseConversion(text) {
+    let result = text
+      .map((string) => string.toLowerCase())
+      .map((string, i) => {
+        return string[0].toUpperCase() + string.substring(1);
+      });
+
+    return result.join("");
+  }
 }
