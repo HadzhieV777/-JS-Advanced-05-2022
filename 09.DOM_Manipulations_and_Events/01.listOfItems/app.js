2536c09561eeb5e7398ea9f5318cbf7aee574424
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-04 16:35:20 +0300

    Modified 02.deleteFromTable/app.js

--- 09.DOM_Manipulations_and_Events/01.listOfItems/app.js
+++ 09.DOM_Manipulations_and_Events/01.listOfItems/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+  let input = document.getElementById("newItemText");
+  let listOfItems = document.getElementById("items");
+
+  let inputValue = input.value;
+  let newLi = document.createElement("li");
+  newLi.textContent = inputValue;
+  listOfItems.appendChild(newLi);
+  input.value = "";
 }
