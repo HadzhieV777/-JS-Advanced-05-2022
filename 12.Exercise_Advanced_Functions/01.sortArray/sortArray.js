6dc42dedfbfceac39ea3e726a65e94b089798910
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-06 13:03:07 +0300

    Added 01.sortArray

---
+++ 12.Exercise_Advanced_Functions/01.sortArray/sortArray.js
@@ -0,0 +1,11 @@
+function solve(arr, order) {
+    if (order === 'asc') {
+        return arr.sort((a, b) => a - b)
+    } else if (order === 'desc') {
+        return arr.sort((a, b) => b - a)
+    } 
+
+}
+
+console.log(solve([14, 7, 17, 6, 8], 'asc')) //  [6, 7, 8, 14, 17]
+console.log(solve([14, 7, 17, 6, 8], 'desc'))//  [17, 14, 8, 7, 6]
