84ae0d09da1742b7d463e45c2073ec7a14aa6ca0
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-07 08:46:45 +0300

    Added 06.formatTheText

---
+++ 08.Exercise_DOM_Introduction/06.formatTheText/solution.js
@@ -0,0 +1,13 @@
+function solve() {
+  let input = document.getElementById("input").value;
+  let output = document.getElementById("output");
+
+  let sentences = input.split(".").filter((s) => s.length != 0);
+
+  while (sentences.length > 0) {
+    let textForParagraph = sentences.splice(0, 3).join(". ") + ".";
+    let paragraph = document.createElement("p");
+    paragraph.textContent = textForParagraph;
+    output.appendChild(paragraph);
+  }
+}
