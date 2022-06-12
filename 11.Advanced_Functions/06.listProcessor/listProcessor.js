3edb39d1a4515878b2d2e4c99bafdbb00c4df4e0
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-12 11:28:31 +0300

    Added 06.listProcessor

---
+++ 11.Advanced_Functions/06.listProcessor/listProcessor.js
@@ -0,0 +1,35 @@
+function solve(arr) {
+  let result = [];
+
+  for (let input of arr) {
+    let [command, string] = input.split(" ");
+
+    switch (command) {
+      case "add":
+        add(string);
+        break;
+      case "remove":
+        remove(string);
+        break;
+      case "print":
+        print();
+        break;
+    }
+
+    function add(string) {
+      result.push(string);
+    }
+
+    function remove(string) {
+      result = result.filter((el) => el !== string);
+    }
+
+    function print() {
+      console.log(result.join(","));
+    }
+  }
+}
+
+["add hello", "add again", "remove hello", "add again", "print"]; // again,again
+
+["add pesho", "add george", "add peter", "remove peter", "print"]; // pesho,george
