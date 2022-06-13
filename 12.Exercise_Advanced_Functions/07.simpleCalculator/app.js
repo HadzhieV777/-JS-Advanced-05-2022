13558008e44f54475822269d47138a288d8243d9
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-13 10:06:23 +0300

    Added 07.simpleCalculator

---
+++ 12.Exercise_Advanced_Functions/07.simpleCalculator/app.js
@@ -0,0 +1,27 @@
+function calculator() {
+    function init(selector1, selector2, resultSelector) {
+        this.num1 = document.querySelector(selector1)
+        this.num2 = document.querySelector(selector2)
+        this.result = document.querySelector(resultSelector)
+    }
+
+    function add() {
+        this.result.value = Number(this.num1.value) + Number(this.num2.value)
+    }
+
+    function subtract() {
+        this.result.value = Number(this.num1.value) - Number(this.num2.value)
+    }
+
+    return {
+        init,
+        add,
+        subtract
+    }
+}
+
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
+
+
+
