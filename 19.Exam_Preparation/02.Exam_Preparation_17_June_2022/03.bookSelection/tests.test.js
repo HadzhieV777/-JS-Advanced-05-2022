7ca57969eae1a6c55429fbe10b702a24089a27b4
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-19 15:21:23 +0300

    Installed mocha and chai

---
+++ 19.Exam_Preparation/02.Exam_Preparation_17_June_2022/03.bookSelection/tests.test.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const { bookSelection } = require("./solution");
+
+
+describe('BookSelection function', () => {
+    describe('TODO', () => {
+        // TODO
+    })
+})
