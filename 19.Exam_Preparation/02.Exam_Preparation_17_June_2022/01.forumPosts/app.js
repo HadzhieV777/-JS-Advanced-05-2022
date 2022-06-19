511d7c867f2773e06e436b8aca9b0a372ccbe82e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-19 13:10:22 +0300

    Edited app.js

--- 19.Exam_Preparation/02.Exam_Preparation_17_June_2022/01.forumPosts/app.js
+++ 19.Exam_Preparation/02.Exam_Preparation_17_June_2022/01.forumPosts/app.js
@@ -1,115 +1,155 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  function solve() {
-    const titleInputEl = document.getElementById("post-title");
-    const categoryInputEl = document.getElementById("post-category");
-    const contentInputEl = document.getElementById("post-content");
-    const reviewUlEl = document.getElementById("review-list");
-    const publishedUlEl = document.getElementById("published-list");
-    const reviewMessage = document.getElementById("reviewed-msg");
-    const uploadedMessage = document.getElementById("approved-msg");
-    document.getElementById("clear-btn").addEventListener("click", clearPosts);
-    const submitButtonEl = document
-      .getElementById("publish-btn")
-      .addEventListener("click", (ev) => {
-        if (
-          titleInputEl.value !== "" &&
-          categoryInputEl.value !== "" &&
-          contentInputEl.value !== ""
-        ) {
-          addPost(
-            ev,
-            titleInputEl.value,
-            categoryInputEl.value,
-            contentInputEl.value
-          );
-          clearInputFileds();
-        }
-      });
+  // Fields
+  let postTitleField = document.getElementById("post-title");
+  let postCategoryField = document.getElementById("post-category");
+  let postContentField = document.getElementById("post-content");
 
-    function addPost(ev, titleInputEl, categoryInputEl, contentInputEl) {
-      // ev.preventDefault();
+  // Related posts and published posts lists
+  let relatedPostsContainer = document.getElementById("relatedPosts");
+  let publishedList = document.getElementById("published-list");
 
-      const li = elGenerator("li");
-      li.setAttribute("class", "rpost");
-      const article = elGenerator("article", "", li);
-      elGenerator("h4", `${titleInputEl}`, article);
-      elGenerator("p", `Category: ${categoryInputEl}`, article);
-      elGenerator("p", `Content: ${contentInputEl}`, article);
+  // Main buttons
+  let publishBtn = document.getElementById("publish-btn");
+  let clearBtn = document.getElementById("clear-btn");
 
-      const approveBtn = elGenerator("button", "Approve", li);
-      approveBtn.setAttribute("class", "action-btn approve");
-      // approveBtn.setAttribute("class", "approve");
-      const editBtn = elGenerator("button", "Edit", li);
-      editBtn.setAttribute("class", "action-btn edit");
-      // editBtn.setAttribute("class", "edit");
+  publishBtn.addEventListener("click", () => {
+    // Read the input fields
+    let title = postTitleField.value;
+    let category = postCategoryField.value;
+    let content = postContentField.value;
 
-      reviewUlEl.appendChild(li);
-
-      if (reviewUlEl.children.length !== 0) {
-        reviewMessage.style.display = "none";
-      }
-
-      approveBtn.addEventListener("click", (ev) => approvePost(ev, article));
-      editBtn.addEventListener("click", (ev) =>
-        editPost(ev, titleInputEl, categoryInputEl, contentInputEl)
-      );
+    // Validate the input fields
+    if (title == "" || category == "" || content == "") {
+      return;
     }
 
-    function editPost(ev, _titleInputEl, _categoryInputEl, _contentInputEl) {
-      ev.target.parentNode.remove();
+    //  create list items
+    let newLiElement = document.createElement("li");
+    newLiElement.className = "rpost";
+    newLiElement.innerHTML = `<article>
+      <h4>${title}</h4>
+      <p>Category: ${category}</p>
+      <p>Content: ${content}</p>
+      </article>
+      <button class="action-btn edit">Edit</button>
+      <button class="action-btn approve">Approve</button>
+      `;
 
-      titleInputEl.value = _titleInputEl;
-      categoryInputEl.value = _categoryInputEl;
-      contentInputEl.value = _contentInputEl;
+    // Add functionality to the secondary buttons
+    let editBtn = newLiElement.querySelector(".edit");
+    let approveBtn = newLiElement.querySelector(".approve");
+    editBtn.addEventListener("click", editPost);
+    approveBtn.addEventListener("click", approvePost);
+
+    // Append the new Li to the related posts container
+    relatedPostsContainer.appendChild(newLiElement);
+
+    // Clear the fields
+    postTitleField.value = "";
+    postCategoryField.value = "";
+    postContentField.value = "";
+
+    function editPost() {
+      // Populate input fields
+      postTitleField.value = title;
+      postCategoryField.value = category;
+      postContentField.value = content;
+
+      // Remove the list item
+      newLiElement.remove();
     }
 
-    function approvePost(ev, articleEl) {
-      ev.target.parentNode.remove();
+    function approvePost() {
+      // move the list item from the first to the second list
+      publishedList.appendChild(newLiElement);
 
-      const publishedLiEl = document.createElement("li");
-      publishedLiEl.className = "rpost";
-      publishedLiEl.appendChild(articleEl);
-      publishedUlEl.appendChild(publishedLiEl);
-
-      if (publishedUlEl.children.length !== 0) {
-        uploadedMessage.style.display = "none";
-      }
-
-      if (reviewUlEl.children.length == 0) {
-        reviewMessage.style.display = "block";
-      }
+      // remove buttons
+      editBtn.remove();
+      approveBtn.remove();
     }
+  });
 
-    function clearPosts() {
-      // if (publishedUlEl.children.length > 0) {
-      //   publishedUlEl.innerHTML = "";
-      //   uploadedMessage.style.display = "block";
-      // }
-      let postsArray = Array.from(publishedUlEl.children);
-      postsArray.forEach((post) => {
-        post.remove();
-      });
-      if (publishedUlEl.children.length == 0) {
-        uploadedMessage.style.display = "block";
-      }
-    }
-
-    function clearInputFileds() {
-      titleInputEl.value = "";
-      categoryInputEl.value = "";
-      contentInputEl.value = "";
-    }
-
-    function elGenerator(type, content, parent) {
-      const element = document.createElement(type);
-      element.textContent = content;
-
-      if (parent) {
-        parent.appendChild(element);
-      }
-      return element;
-    }
-  }
+  clearBtn.addEventListener("click", () => {
+    // clear the uploaded posts section
+    publishedList.innerHTML = "";
+  });
 }
+
+// Second way with objects
+// function solve() {
+//   const input = {
+//       title: document.getElementById('post-title'),
+//       category: document.getElementById('post-category'),
+//       content: document.getElementById('post-content')
+//   };
+//   const lists = {
+//       review: document.getElementById('review-list'),
+//       published: document.getElementById('published-list')
+//   };
+//   document.getElementById('publish-btn').addEventListener('click', publish);
+//   document.getElementById('clear-btn').addEventListener('click', clear);
+
+//   function publish(event) {
+//       event.preventDefault();
+
+//       // read input fields
+//       const title = input.title.value;
+//       const category = input.category.value;
+//       const content = input.content.value;
+
+//       // validate input
+//       if (title == '' || category == '' || content == '') {
+//           return;
+//       }
+
+//       // create list item
+//       const li = document.createElement('li');
+//       li.className = 'rpost';
+//       li.innerHTML = `<article>
+//           <h4>${title}</h4>
+//           <p>Category: ${category}</p>
+//           <p>Content: ${content}</p>
+//       </article>
+//       <button class="action-btn edit">Edit</button>
+//       <button class="action-btn approve">Approve</button>`;
+
+//       // * add functionality to buttons
+//       const editBtn = li.querySelector('.edit');
+//       const approveBtn = li.querySelector('.approve');
+//       editBtn.addEventListener('click', edit);
+//       approveBtn.addEventListener('click', approve);
+
+//       // append to first list
+//       lists.review.appendChild(li);
+
+//       // clear input fields
+//       input.title.value = '';
+//       input.category.value = '';
+//       input.content.value = '';
+
+//       function edit() {
+//           // populate input fields with values
+//           input.title.value = title;
+//           input.category.value = category;
+//           input.content.value = content;
+
+//           // remove list item from list
+//           li.remove();
+//       }
+
+//       function approve() {
+//           // move list item from first list to second list
+//           lists.published.appendChild(li);
+//           // remove action buttons
+//           editBtn.remove();
+//           approveBtn.remove();
+//       }
+//   }
+
+//   function clear() {
+//       // set second list HTML to empty string
+//       lists.published.innerHTML = '';
+//   }
+// }
