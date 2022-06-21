d7f999de8e3ff478879c8c065d535ec43da9c339
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-21 11:26:59 +0300

    Edited solution.js

--- 19.Exam_Preparation/05.Retake_Exam_10_December_2019/03.christmasDinner/solution.js
+++ 19.Exam_Preparation/05.Retake_Exam_10_December_2019/03.christmasDinner/solution.js
@@ -1,3 +1,16 @@
 class ChristmasDinner {
-    // TODO: implement this class...
+    constructor(budget) {
+        this.budget = budget
+        this.dishes = []
+        this.products = []
+        this.guests = {}
+    }
+
+    shopping([product]) {}
+
+    recipes({recipe}) {}
+
+    inviteGuests(name, dish) {}
+
+    showAttendance() {}
 }
