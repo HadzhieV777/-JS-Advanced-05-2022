f9397fd3dc11d67c863c2af0d44cac79470218b0
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-21 11:30:50 +0300

    Edited solution.js

--- 19.Exam_Preparation/05.Retake_Exam_10_December_2019/03.christmasDinner/solution.js
+++ 19.Exam_Preparation/05.Retake_Exam_10_December_2019/03.christmasDinner/solution.js
@@ -1,16 +1,28 @@
 class ChristmasDinner {
-    constructor(budget) {
-        this.budget = budget
-        this.dishes = []
-        this.products = []
-        this.guests = {}
+  constructor(budget) {
+    this.budget = budget;
+    this.dishes = [];
+    this.products = [];
+    this.guests = {};
+  }
+
+  set budget(budget) {
+    if (budget < 0) {
+      throw new Error("The budget cannot be a negative number");
     }
 
-    shopping([product]) {}
+    this._budget = budget;
+  }
 
-    recipes({recipe}) {}
+  get budget() {
+    return this._budget;
+  }
 
-    inviteGuests(name, dish) {}
+  shopping(product) {}
 
-    showAttendance() {}
+  recipes(recipe) {}
+
+  inviteGuests(name, dish) {}
+
+  showAttendance() {}
 }
