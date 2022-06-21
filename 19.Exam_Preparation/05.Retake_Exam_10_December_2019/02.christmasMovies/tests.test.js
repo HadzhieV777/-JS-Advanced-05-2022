c149c862a5eb8656cb2945e3e185c777e99ff900
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-21 12:23:07 +0300

    Edited tests.test.js

--- 19.Exam_Preparation/05.Retake_Exam_10_December_2019/02.christmasMovies/tests.test.js
+++ 19.Exam_Preparation/05.Retake_Exam_10_December_2019/02.christmasMovies/tests.test.js
@@ -1,8 +1,11 @@
-describe("Tests …", function() {
-    describe("TODO …", function() {
-        it("TODO …", function() {
-            // TODO: …
-        });
-     });
-     // TODO: …
+const { expect } = require("chai");
+const { ChristmasMovies } = require("./solution");
+
+describe("Tests …", function () {
+  describe("TODO …", function () {
+    it("TODO …", function () {
+      // TODO: …
+    });
+  });
+  // TODO: …
 });
