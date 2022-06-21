2e25ee13ec2a25f86e615c6036129ba8befcf66b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-21 11:48:33 +0300

    Edited solution.js

--- 19.Exam_Preparation/05.Retake_Exam_10_December_2019/03.christmasDinner/solution.js
+++ 19.Exam_Preparation/05.Retake_Exam_10_December_2019/03.christmasDinner/solution.js
@@ -10,7 +10,6 @@ class ChristmasDinner {
     if (budget < 0) {
       throw new Error("The budget cannot be a negative number");
     }
-
     this._budget = budget;
   }
 
@@ -18,11 +17,46 @@ class ChristmasDinner {
     return this._budget;
   }
 
-  shopping(product) {}
+  shopping(product) {
+    const type = product[0];
+    const price = product[1];
 
-  recipes(recipe) {}
+    if (price > this.budget) {
+      throw new Error("Not enough money to buy this product");
+    }
+
+    this.products.push(type);
+    this.budget -= price;
+    return `You have successfully bought ${type}!`;
+  }
+
+  recipes(recipe) {
+    let allProductsAvailable = true;
+
+    for (let product of recipe.productsList) {
+      if (this.products.indexOf(product) === -1) {
+        allProductsAvailable = false;
+      }
+    }
+
+    if (!allProductsAvailable) {
+      throw new Error("We do not have this product");
+    }
+
+    this.dishes.push(recipe);
+    return `${recipe.recipeName} has been successfully cooked!`;
+  }
 
   inviteGuests(name, dish) {}
 
   showAttendance() {}
 }
+
+let dinner = new ChristmasDinner(300);
+console.log(dinner.shopping(['Fruits', 1]))
+console.log(dinner.shopping(['Honey', 1]))
+
+console.log(dinner.recipes({
+    recipeName: 'Oshav',
+    productsList: ['Fruits', 'Honey']
+}));
