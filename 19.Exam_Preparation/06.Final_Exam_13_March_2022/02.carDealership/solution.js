ba681f808b63cba3008f29f4b72ff41900003102
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-22 09:05:32 +0300

    Edited solution.js

---
+++ 19.Exam_Preparation/06.Final_Exam_13_March_2022/02.carDealership/solution.js
@@ -0,0 +1,13 @@
+class CarDealership {}
+
+// Input
+let dealership = new CarDealership("SoftAuto");
+console.log(dealership.addCar("Toyota Corolla", 100, 3500, 190000));
+console.log(dealership.addCar("Mercedes C63", 300, 29000, 187000));
+console.log(dealership.addCar("", 120, 4900, 240000));
+
+// Output
+
+// New car added: Toyota Corolla - 100 HP - 190000.00 km - 3500.00$
+// New car added: Mercedes C63 - 300 HP - 187000.00 km - 29000.00$
+// Uncaught Error Error: Invalid input!
