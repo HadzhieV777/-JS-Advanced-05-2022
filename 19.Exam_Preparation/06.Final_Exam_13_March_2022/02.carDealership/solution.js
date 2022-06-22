b2ceec3eae2d7cc72213c499019936ec71bb1b65
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-22 09:38:33 +0300

    Edited solution.js

--- 19.Exam_Preparation/06.Final_Exam_13_March_2022/02.carDealership/solution.js
+++ 19.Exam_Preparation/06.Final_Exam_13_March_2022/02.carDealership/solution.js
@@ -26,7 +26,37 @@ class CarDealership {
     )} km - ${price.toFixed(2)}$`;
   }
 
-  sellCar(model, desiredMileage) {}
+  sellCar(model, desiredMileage) {
+    // find the wanted car
+    const wantedCarIndex = this.availableCars.findIndex(
+      (c) => c.model === model
+    );
+    // throw if can't find it
+    if (wantedCarIndex == -1) {
+      throw new Error(`${model} was not found!`);
+    }
+
+    const wantedCar = this.availableCars[wantedCarIndex];
+    let soldPrice = wantedCar.price;
+    const carMileage = wantedCar.mileage;
+
+    // validate the mileage
+    if (carMileage > desiredMileage && carMileage - desiredMileage <= 40000) {
+      soldPrice *= 0.95;
+    } else if (carMileage - desiredMileage > 40000) {
+      soldPrice *= 0.9;
+    }
+
+    // sell
+    this.availableCars.splice(wantedCarIndex, 1);
+    this.soldCars.push({
+      model: wantedCar.model,
+      horsepower: wantedCar.horsepower,
+      soldPrice,
+    });
+    this.totalIncome += soldPrice;
+    return `${wantedCar.model} was sold for ${soldPrice.toFixed(2)}$`;
+  }
 
   currentCar() {}
 
@@ -35,12 +65,13 @@ class CarDealership {
 
 // Input
 let dealership = new CarDealership("SoftAuto");
-console.log(dealership.addCar("Toyota Corolla", 100, 3500, 190000));
-console.log(dealership.addCar("Mercedes C63", 300, 29000, 187000));
-console.log(dealership.addCar("", 120, 4900, 240000));
+dealership.addCar("Toyota Corolla", 100, 3500, 190000);
+dealership.addCar("Mercedes C63", 300, 29000, 187000);
+dealership.addCar("Audi A3", 120, 4900, 240000);
+console.log(dealership.sellCar("Toyota Corolla", 230000));
+console.log(dealership.sellCar("Mercedes C63", 110000));
 
 // Output
 
-// New car added: Toyota Corolla - 100 HP - 190000.00 km - 3500.00$
-// New car added: Mercedes C63 - 300 HP - 187000.00 km - 29000.00$
-// Uncaught Error Error: Invalid input!
+// Toyota Corolla was sold for 3500.00$
+// Mercedes C63 was sold for 26100.00$
