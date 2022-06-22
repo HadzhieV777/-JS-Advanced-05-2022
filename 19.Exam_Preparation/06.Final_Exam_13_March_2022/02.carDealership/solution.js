889a82c1f671adedc50cea19b0db64a285c00528
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-22 10:13:36 +0300

    Edited solution.js

--- 19.Exam_Preparation/06.Final_Exam_13_March_2022/02.carDealership/solution.js
+++ 19.Exam_Preparation/06.Final_Exam_13_March_2022/02.carDealership/solution.js
@@ -63,23 +63,50 @@ class CarDealership {
       return "There are no available cars";
     }
 
-    let cars = this.availableCars.map(c => `---${c.model} - ${c.horsepower} HP - ${c.mileage.toFixed(2)} km - ${c.price.toFixed(2)}$`)
-    return `-Available cars:\n${cars.join('\n')}`
+    let cars = this.availableCars.map(
+      (c) =>
+        `---${c.model} - ${c.horsepower} HP - ${c.mileage.toFixed(
+          2
+        )} km - ${c.price.toFixed(2)}$`
+    );
+    return `-Available cars:\n${cars.join("\n")}`;
   }
 
-  salesReport(criteria) {}
+  salesReport(criteria) {
+    if (criteria !== "horsepower" && criteria !== "model") {
+      throw new Error("Invalid criteria!");
+    }
+    let cars;
+    if (criteria === "horsepower") {
+      cars = this.soldCars.sort((a, b) => b.horsepower - a.horsepower);
+    } else if (criteria === "model") {
+      cars = this.soldCars.sort((a, b) => a.model.localeCompare(b.model));
+    }
+
+    cars = cars.map(
+      (c) => `---${c.model} - ${c.horsepower} HP - ${c.soldPrice.toFixed(2)}$`
+    );
+    if (cars.length > 0) {
+      cars.unshift("");
+    }
+    return `-${this.name} has a total income of ${this.totalIncome.toFixed(
+      2
+    )}$\n-${this.soldCars.length} cars sold:${cars.join("\n")}`;
+  }
 }
 
 // Input
-let dealership = new CarDealership('SoftAuto');
-dealership.addCar('Toyota Corolla', 100, 3500, 190000);
-dealership.addCar('Mercedes C63', 300, 29000, 187000);
-dealership.addCar('Audi A3', 120, 4900, 240000);
-console.log(dealership.currentCar());
+let dealership = new CarDealership("SoftAuto");
+dealership.addCar("Toyota Corolla", 100, 3500, 190000);
+dealership.addCar("Mercedes C63", 300, 29000, 187000);
+dealership.addCar("Audi A3", 120, 4900, 240000);
+dealership.sellCar("Toyota Corolla", 230000);
+dealership.sellCar("Mercedes C63", 110000);
+console.log(dealership.salesReport("horsepower"));
 
 // Output
 
-// -Available cars:
-// ---Toyota Corolla - 100 HP - 190000.00 km - 3500.00$
-// ---Mercedes C63 - 300 HP - 187000.00 km - 29000.00$
-// ---Audi A3 - 120 HP - 240000.00 km - 4900.00$
+// -SoftAuto has a total income of 29600.00$
+// -2 cars sold:
+// ---Mercedes C63 - 300 HP - 26100.00$
+// ---Toyota Corolla - 100 HP - 3500.00$
