19df4de259815e7f4ac611756bf1e1d5c2c5ea83
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-22 15:04:27 +0300

    Edited tests.test.js

--- 19.Exam_Preparation/06.Final_Exam_13_March_2022/03.rentCar/tests.test.js
+++ 19.Exam_Preparation/06.Final_Exam_13_March_2022/03.rentCar/tests.test.js
@@ -1,11 +1,19 @@
 const { expect } = require("chai");
 const { rentCar } = require("./solution");
 
-describe("Tests …", function () {
-  describe("TODO …", function () {
-    it("TODO …", function () {
-      // TODO: …
+describe("rentCar object", () => {
+  describe("searchCar function", () => {
+    it("validates the input and throws", () => {
+        expect(() => rentCar.searchCar({}, 'Audi')).to.throw("Invalid input!")
+        expect(() => rentCar.searchCar([], {})).to.throw("Invalid input!")
     });
+
+    it("check if car model in car shop and throw if not", () => {
+        expect(() => rentCar.searchCar([], "Audi")).to.throw('There are no such models in the catalog!')
+        expect(() => rentCar.searchCar(["BMW", "Mercedes za mnogo palki"], "Audi")).to.throw('There are no such models in the catalog!')
+    })
+
+    it("finds the desired car model and returns", () => {})
   });
-  // TODO: …
+  
 });
