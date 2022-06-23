73f3bf22b1a564149966f269e7a1801c8b510aed
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-23 16:10:57 +0300

    Added 03.cinema

---
+++ 19.Exam_Preparation/08.Final_Retake_Exam_12_August_2021/03.cinema/tests.test.js
@@ -0,0 +1,11 @@
+const { expect } = require("chai");
+const { cinema } = require("./solution");
+
+describe("Tests …", function () {
+  describe("TODO …", function () {
+    it("TODO …", function () {
+      // TODO: …
+    });
+  });
+  // TODO: …
+});
