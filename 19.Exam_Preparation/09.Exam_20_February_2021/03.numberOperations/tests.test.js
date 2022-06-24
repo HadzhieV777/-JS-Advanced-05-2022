610987aff95e4e3507a21aace2de7f7ccefcf7f5
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-24 14:32:09 +0300

    Edited tests.test.js

--- 19.Exam_Preparation/09.Exam_20_February_2021/03.numberOperations/tests.test.js
+++ 19.Exam_Preparation/09.Exam_20_February_2021/03.numberOperations/tests.test.js
@@ -1,11 +1,53 @@
 const { expect } = require("chai");
 const { numberOperations } = require("./solution");
 
-describe("Tests …", function () {
-  describe("TODO …", function () {
-    it("TODO …", function () {
-      // TODO: …
+describe("numberOperations object", () => {
+  describe("powNumber function", () => {
+    it("returns the power of the given number", () => {
+      expect(numberOperations.powNumber(5)).to.equal(25);
     });
   });
-  // TODO: …
+
+  describe("numberChecker function", () => {
+    it("parses the input to number, and validates it", () => {
+      expect(() => numberOperations.numberChecker("invalid")).to.throw(
+        "The input is not a number!"
+      );
+      expect(() => numberOperations.numberChecker({})).to.throw(
+        "The input is not a number!"
+      );
+      expect(() => numberOperations.numberChecker()).to.throw(
+        "The input is not a number!"
+      );
+    });
+
+    it("checks if the number < 100 and returns", () => {
+      expect(numberOperations.numberChecker("20")).to.equal(
+        "The number is lower than 100!"
+      );
+      expect(numberOperations.numberChecker(20)).to.equal(
+        "The number is lower than 100!"
+      );
+      expect(numberOperations.numberChecker(100)).to.equal(
+        "The number is greater or equal to 100!"
+      );
+      expect(numberOperations.numberChecker(101)).to.equal(
+        "The number is greater or equal to 100!"
+      );
+    });
+  });
+
+  describe("sumArrays function", () => {
+    it("returns new array which represents the sum of the two arrays by indexes", () => {
+      expect(numberOperations.sumArrays([1, 2, 3], [1, 2, 3])).to.deep.equal([
+        2, 4, 6,
+      ]);
+      expect(numberOperations.sumArrays([1, 2, 3], [1, 2, 3, 3])).to.deep.equal(
+        [2, 4, 6, 3]
+      );
+      expect(numberOperations.sumArrays([1, 2, 3, 3], [1, 2, 3])).to.deep.equal(
+        [2, 4, 6, 3]
+      );
+    });
+  });
 });
