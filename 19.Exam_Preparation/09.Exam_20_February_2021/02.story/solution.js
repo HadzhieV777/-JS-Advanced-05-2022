52a8ffe4ec66a6696c35e987e484c93855481cb7
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-24 12:03:56 +0300

    Edited solution.js

--- 19.Exam_Preparation/09.Exam_20_February_2021/02.story/solution.js
+++ 19.Exam_Preparation/09.Exam_20_February_2021/02.story/solution.js
@@ -1,31 +1,60 @@
 class Story {
-    onstructor(title, creator)
+    _comments = []
+    _likes = []
 
-    like (username)
+    onstructor(title, creator) {
+        this.title = title
+        this.creator = creator
+    }
 
-    dislike (username)
+    get likes() {
+        if (this._likes.length === 0) {
+            return `${this.title} has 0 likes`;
+        }
 
-    comment (username, content, id)
+        if (this._likes.length === 1) {
+            return `${this._likes[0]} likes this story!`;
+        }
 
-    toString(sortingType)
+        return `${this._likes[0]} and ${this._likes.length - 1} others like this story!`;
+    }
+
+    like (username) {
+        if (this._likes.indexOf(username) !== -1) {
+            throw new Error("You can't like the same story twice!")
+        }
+
+        if (this.creator === username) {
+            throw new Error("You can't like your own story!")
+        }
+
+        this._likes.push(username)
+        return `${username} liked ${this.title}!`
+    }
+
+    dislike (username) {}
+
+    comment (username, content, id) {}
+
+    toString(sortingType) {}
 }
 
 // Input
 let art = new Story("My Story", "Anny");
 art.like("John");
 console.log(art.likes);
-art.dislike("John");
-console.log(art.likes);
-art.comment("Sammy", "Some Content");
-console.log(art.comment("Ammy", "New Content"));
-art.comment("Zane", "Reply", 1);
-art.comment("Jessy", "Nice :)");
-console.log(art.comment("SAmmy", "Reply@", 1));
-console.log();
-console.log(art.toString("username"));
-console.log();
-art.like("Zane");
-console.log(art.toString("desc"));
+// art.dislike("John");
+// console.log(art.likes);
+// art.comment("Sammy", "Some Content");
+// console.log(art.comment("Ammy", "New Content"));
+// art.comment("Zane", "Reply", 1);
+// art.comment("Jessy", "Nice :)");
+// console.log(art.comment("SAmmy", "Reply@", 1));
+// console.log();
+// console.log(art.toString("username"));
+// console.log();
+// art.like("Zane");
+// console.log(art.toString("desc"));
 
 // Output
 // John likes this story!
