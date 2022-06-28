ed868d6f0b5908a942df8b8f5715ba050a75c2d1
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-06-28 10:38:01 +0300

    Added 16.viewModel

---
+++ 16.Exercise_Classes/11.viewModel/app.js
@@ -0,0 +1,33 @@
+class Textbox {
+  constructor(selector, regex) {
+    this.selector = selector;
+    this._invalidSymbols = regex;
+
+    this._elements = document.querySelectorAll(selector);
+    this._elements[0].addEventListener("input", () => {
+      this._elements[1].value = this._elements[0].value;
+      this._value = this._elements[0].value;
+    });
+  }
+  get value() {
+    return this._value;
+  }
+  set value(newValue) {
+    Array.from(this._elements).forEach((el) => (el.value = newValue));
+    this._value = newValue;
+  }
+
+  get elements() {
+    return this._elements;
+  }
+  isValid() {
+    return !this.value.match(this._invalidSymbols);
+  }
+}
+
+let textbox = new Textbox(".textbox", /[^a-zA-Z0-9]/);
+let inputs = document.getElementsByClassName(".textbox");
+
+inputs.addEventListener("click", function () {
+  console.log(textbox.value);
+});
